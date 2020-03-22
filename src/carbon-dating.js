870512c0aa0910deee1549c384e9ba2dd3d2a253
Artur Gatatdinov <somna.dev@gmail.com>
2020-03-22 20:48:08 +0500

    solved carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,9 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    let sampleActivityParse = parseFloat(sampleActivity);
+    if (sampleActivityParse > MODERN_ACTIVITY ||
+        sampleActivityParse <= 0 || (typeof sampleActivity != "string") || !isFinite(sampleActivityParse)) return false;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityParse) / (0.693 / HALF_LIFE_PERIOD));
 };
